f97f234ec8e1735b8b65f5981136ac2d1067aaa2
Nico Bodkin <nico@MacBook-Pro.local>
2019-04-11 16:10:41 -0400

    structure update

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -1,94 +1,78 @@
 # coding: utf-8
-import re 
-#import tweepy 
-#from tweepy import OAuthHandler 
-from textblob import TextBlob 
+import re
+#import tweepy
+#from tweepy import OAuthHandler
+from textblob import TextBlob
 import pandas
 import csv
 
 
-class TwitterClient(object): 
+def clean_tweet(tweet):
+    ''' 
+    Utility function to clean tweet text by removing links, special characters 
+    using simple regex statements. 
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
 
-    def clean_tweet(self, tweet): 
-        ''' 
-        Utility function to clean tweet text by removing links, special characters 
-        using simple regex statements. 
-        '''
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split()) 
-  
-    def get_tweet_sentiment(self, tweet): 
-        ''' 
-        Utility function to classify sentiment of passed tweet 
-        using textblob's sentiment method 
-        '''
-        # create TextBlob object of passed tweet text 
-        analysis = TextBlob(self.clean_tweet(tweet)) 
-        # set sentiment 
-        '''
-        if analysis.sentiment.polarity > 0: 
-            return 'positive'
-        elif analysis.sentiment.polarity == 0: 
-            return 'neutral'
+
+def get_tweet_sentiment(tweet):
+    ''' 
+    Utility function to classify sentiment of passed tweet 
+    using textblob's sentiment method 
+    '''
+    # create TextBlob object of passed tweet text
+    analysis = TextBlob(clean_tweet(tweet))
+
+    return [analysis.polarity, analysis.subjectivity]
+
+
+def get_tweets():
+    ''' 
+    Main function to fetch tweets and parse them. 
+    '''
+
+    # empty list to store parsed tweets
+    #original_tweets = pandas.read_csv("a.csv", header = None, delimiter="\t", quoting=csv.QUOTE_NONE, encoding='utf-8')
+    #print(tweets.loc[: , "text"])
+
+    # file input here!!!! KEVIN!
+    # list of dictionaries formatted like fetched_tweets = [{"text":"blah blah blah", "rt": 50, "username":"'}, {"text":"blah blah blah", "rt": 50, "username":"'}, ...]
+
+    original_tweets = [{'text': "I can’t believe you had the balls to tweet this. #LiarInChief #ImpeachTrumpNow"}, {'text': "Secure our nations boarder!!!"}, {'text': "We all are waiting for you to go to jail"}, {'text': "The first contracts to support the construction of President Donald Trump's border wall are expected to be awarded this week using Pentagon funds"}, {'text': "Check this out - TRUTH!"}, {'text': "“What’s completely unacceptable is for Congesswoman Omar to target Jews, in this case Stephen Miller.” Jeff Ballabon, B2 Strategic, CEO.  @Varneyco"},
+                       {'text': "On National Former Prisoner of War Recognition Day, we honor the Americans captured and imprisoned by foreign powers while carrying out their duties to defend this great Nation..."}, {'text': "The Mainstream Media has never been more inaccurate or corrupt than it is today. It only seems to get worse. So much Fake News!"}, {'text': "The Democrats will never be satisfied, no matter what they get, how much they get, or how many pages they get. It will never end, but that’s the way life goes!"}]
+
+    tweets = []
+    # parsing tweets one by one
+    for tweet in original_tweets:
+
+        # saving sentiment of tweet
+        tweet['sentiment'], tweet['subjectivity'] = get_tweet_sentiment(tweet['text'])
+        
+        tweets.append(tweet)
+
+        ''' #exlcluding rt part
+        # appending parsed tweet to tweets list 
+        if tweet['rt_count'] > 0: 
+            # if tweet has retweets, ensure that it is appended only once 
+            if tweet not in tweets: 
+                    tweets.append(tweet) 
         else: 
-            return 'negative'
+            tweets.append(tweet) 
+		'''
 
-        '''
-        return [analysis.polarity, analysis.subjectivity]
-
-  
-    def get_tweets(self): 
-        ''' 
-        Main function to fetch tweets and parse them. 
-        '''
-        # empty list to store parsed tweets 
-        tweets = pandas.read_csv("a.csv", header = None, delimiter="\t", quoting=csv.QUOTE_NONE, encoding='utf-8')
-        print(tweets.loc[: , "text"])
-
-  
+    # return parsed tweets with sentiment and subjectivity
+    return tweets
 
 
+def main():
+	# calling function to get tweets
+	tweets = get_tweets()
 
-        #file input here!!!!1
-        #list of dictionaries formatted like fetched_tweets = [{"tweet":"blah blah blah", "rt": 50, "username":"'}, {"tweet":"blah blah blah", "rt": 50, "username":"'}, ...]
+	print ('sentiment: ', 'subjectivity:  ', 'text:')
+	for tweet in tweets:
+		print("  {0:.2f}       {1:.2f}        {2}".format(tweet['sentiment'], tweet['subjectivity'], tweet['text'][:100]))
 
-        fetched_tweets = ["I can’t believe you had the balls to tweet this. #LiarInChief #ImpeachTrumpNow", "Secure our nations boarder!!!", "We all are waiting for you to go to jail", "The first contracts to support the construction of President Donald Trump's border wall are expected to be awarded this week using Pentagon funds", "Check this out - TRUTH!", "“What’s completely unacceptable is for Congesswoman Omar to target Jews, in this case Stephen Miller.” Jeff Ballabon, B2 Strategic, CEO.  @Varneyco", "On National Former Prisoner of War Recognition Day, we honor the Americans captured and imprisoned by foreign powers while carrying out their duties to defend this great Nation...", "The Mainstream Media has never been more inaccurate or corrupt than it is today. It only seems to get worse. So much Fake News!", "The Democrats will never be satisfied, no matter what they get, how much they get, or how many pages they get. It will never end, but that’s the way life goes!"]
-
-
-        # parsing tweets one by one 
-        for tweet in fetched_tweets: 
-            # empty dictionary to store required params of a tweet 
-            parsed_tweet = {} 
-  
-            # saving text of tweet 
-            parsed_tweet['text'] = tweet #tweet.text 
-            # saving sentiment of tweet 
-            parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet) 
-
-  
-            # appending parsed tweet to tweets list 
-            if tweet.retweet_count > 0: 
-                # if tweet has retweets, ensure that it is appended only once 
-                if parsed_tweet not in tweets: 
-                        tweets.append(parsed_tweet) 
-            else: 
-                tweets.append(parsed_tweet) 
-  				
-
-            # return parsed tweets 
-            return tweets 
-  
-  
-def main(): 
-    # creating object of TwitterClient Class 
-    api = TwitterClient() 
-    # calling function to get tweets 
-    tweets = api.get_tweets() 
-
-    print ('polarity:  ', 'subjectivity:  ', 'text:')
-    for tweet in tweets:
-    	print("  {0:.2f}       {1:.2f}        {2}".format(tweet['sentiment'][0], tweet['sentiment'][1], tweet['text'][:100]))
-
-'''
+	'''
     # picking positive tweets from tweets 
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
     # percentage of positive tweets 
@@ -109,8 +93,8 @@ def main():
     print("\n\nNegative tweets:") 
     for tweet in ntweets[:10]: 
         print(tweet['text']) 
-'''
-  
-if __name__ == "__main__": 
-    # calling main function 
-    main() 
+	'''
+
+if __name__ == "__main__":
+    # calling main function
+    main()
