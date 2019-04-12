1ef04df8dbc2dd48bbceaaaaac052228d7117c11
Nico Bodkin <nico@MacBook-Pro.local>
2019-04-12 16:30:06 -0400

    i/o + working

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -4,7 +4,6 @@ import re
 # from tweepy import OAuthHandler
 from textblob import TextBlob
 import pandas
-import csv
 
 
 def clean_tweet(tweet):
@@ -32,34 +31,27 @@ def get_tweets():
     '''
 
     # empty list to store parsed tweets
-    df = pandas.read_csv("tweets.csv", na_values={'user_verified':False}, dtype={'time':str, 'user_screen_name':str, 'text':str, 'tweet_type':str, 'hashtags':str, 'retweet_count':float, 'user_description':str, 'user_followers_count':float, 'user_friends_count':float, 'user_statuses_count':float, 'user_verified':bool})
+    df = pandas.read_csv("tweets.csv", na_values={'user_verified':False}, dtype={'time':str, 'user_screen_name':str, 'text':str, 'tweet_type':str, 'hashtags':str, 'retweet_count':float, 'user_description':str, 'user_followers_count':float, 'user_friends_count':float, 'user_statuses_count':float, 'user_verified':str})
     #original_tweets = pandas.read_csv("a.csv", header = None, delimiter="\t", quoting=csv.QUOTE_NONE, encoding='utf-8')
-    df.to_dict('original_tweets')
+    df.fillna({'user_verified':False})
+    original_tweets = df.to_dict('records')
 
     # list of dictionaries formatted like fetched_tweets = [{"text":"blah blah blah", "rt": 50, "username":"'}, {"text":"blah blah blah", "rt": 50, "username":"'}, ...]
-
-    #original_tweets = [{'text': "I can’t believe you had the balls to tweet this. #LiarInChief #ImpeachTrumpNow", 'sentiment': 1}, {'text': "Secure our nations boarder!!!"}, {'text': "We all are waiting for you to go to jail"}, {'text': "The first contracts to support the construction of President Donald Trump's border wall are expected to be awarded this week using Pentagon funds"}, {'text': "Check this out - TRUTH!"}, {'text': "“What’s completely unacceptable is for Congesswoman Omar to target Jews, in this case Stephen Miller.” Jeff Ballabon, B2 Strategic, CEO.  @Varneyco"},
-    #    {'text': "On National Former Prisoner of War Recognition Day, we honor the Americans captured and imprisoned by foreign powers while carrying out their duties to defend this great Nation..."}, {'text': "The Mainstream Media has never been more inaccurate or corrupt than it is today. It only seems to get worse. So much Fake News!"}, {'text': "The Democrats will never be satisfied, no matter what they get, how much they get, or how many pages they get. It will never end, but that’s the way life goes!"}]
 
     tweets = []
     # parsing tweets one by one
     for tweet in original_tweets:
 
         # saving sentiment of tweet
-        tweet['sentiment'], tweet['subjectivity'] = get_tweet_sentiment(
-            tweet['text'])
+        tweet['sentiment'], tweet['subjectivity'] = get_tweet_sentiment(tweet['text'])
 
-        tweets.append(tweet)
-
-        ''' #exlcluding rt part
         # appending parsed tweet to tweets list
-        if tweet['rt_count'] > 0:
+        if tweet['retweet_count'] > 0:
             # if tweet has retweets, ensure that it is appended only once
             if tweet not in tweets:
                     tweets.append(tweet)
         else:
             tweets.append(tweet)
-        '''
 
     # return parsed tweets with sentiment and subjectivity
     return tweets
@@ -69,9 +61,11 @@ def main():
     # calling function to get tweets
     tweets = get_tweets()
 
+    '''
     print ('sentiment: ', 'subjectivity:  ', 'text:')
     for tweet in tweets:
         print("  {0:.2f}       {1:.2f}        {2}".format(tweet['sentiment'], tweet['subjectivity'], tweet['text'][:100]))
+    '''
 
     print('\nexported to csv ;)')
     df = pandas.DataFrame(tweets)
